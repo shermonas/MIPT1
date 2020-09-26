50d075fb4f518388559956841c17b108438b9dfc
shermonas <viliuunas@gmail.com>
2020-09-26 21:22:46 +0300

    Pirmasis mygtukas tekstui pakeisti

--- app/src/main/java/com/example/mipt1/MainActivity.java
+++ app/src/main/java/com/example/mipt1/MainActivity.java
@@ -3,12 +3,20 @@ package com.example.mipt1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView tvMain;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.tvMain = findViewById(R.id.tvMain);
+    }
+
+    public void onBtnChangeTextClick(View view) {
+        this.tvMain.setText("Gražios dienos");
     }
 }
